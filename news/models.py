bf2d386fa3913b6a4e63380c851c619e8c11c9e6
nahidsaikat <nahidsaikatft40@gmail.com>
2020-04-18 23:45:55 +0600

    Add published_at column

--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ class HeadLine(models.Model):
     title = models.CharField(max_length=200)
     image = models.URLField(null=True, blank=True)
     url = models.TextField()
+    published_at = models.CharField(max_length=32, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
