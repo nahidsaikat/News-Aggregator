27e866fd63d5151f5816644cfd0d27ecd32237ce
nahidsaikat <nahidsaikatft40@gmail.com>
2020-04-18 23:44:21 +0600

    Add created_at and updated_at field

--- news/models.py
+++ news/models.py
@@ -5,6 +5,8 @@ class HeadLine(models.Model):
     title = models.CharField(max_length=200)
     image = models.URLField(null=True, blank=True)
     url = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
