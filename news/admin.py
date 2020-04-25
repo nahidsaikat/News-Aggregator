43a762e0ae6c50a25870ce556940f06bb1690a08
nahidsaikat <nahidsaikatft40@gmail.com>
2020-04-25 15:09:27 +0600

    Update admin.py to show more column in News Provider model

--- news/admin.py
+++ news/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from news.models import HeadLine, NewsProvider
 
 
+class NewsProviderAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'code')
+
+
 admin.site.register(HeadLine)
-admin.site.register(NewsProvider)
+admin.site.register(NewsProvider, NewsProviderAdmin)
