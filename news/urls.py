62842bdc5bc84f74de88beb27a249bde2fbfd30d
nahidsaikat <nahidsaikatft40@gmail.com>
2020-05-05 06:05:54 +0600

    Add the search feature

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import scrape, news_list, mark_read
+from news.views import scrape, news_list, mark_read, search
 
 urlpatterns = [
+  path('search/', search, name="search"),
   path('scrape/', scrape, name="scrape"),
   path('mark_read/<int:pk>/', mark_read, name="mark_read"),
   path('', news_list, name="home"),
